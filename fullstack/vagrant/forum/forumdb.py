e931e97d7f65784b24ce4637ceab990a7396190b
Jeff Reiher <jeffreiher@gmail.com>
2015-09-11 13:20:48 -0400

    add sqlite3 code to getposts and to add posts to and fromdb

--- fullstack/vagrant/forum/forumdb.py
+++ fullstack/vagrant/forum/forumdb.py
@@ -3,29 +3,28 @@
 # 
 
 import time
+import sqlite3
 
 ## Database connection
-DB = []
+# DB = []
+
 
 ## Get posts from database.
 def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
-
-    Returns:
-      A list of dictionaries, where each dictionary has a 'content' key
-      pointing to the post content, and 'time' key pointing to the time
-      it was posted.
-    '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    DB = sqlite3.connect("forum.db")
+    c = DB.cursor()
+    c.execute("SELECT time, content FROM posts ORDER BY time DESC")
+    posts = ({'content': str(row[1]), 'time': str(row[0])} for row in c.fetchall())
+    # posts.sort(key=lambda row: row['time'], reverse=True)
+    DB.close()
     return posts
 
 ## Add a post to the database.
 def AddPost(content):
-    '''Add a new post to the database.
-
-    Args:
-      content: The text content of the new post.
-    '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    DB = sqlite3.connect("forum.db")
+    c = DB.cursor()
+    # t = time.strftime('%c', time.localtime())
+    # DB.append((t, content))
+    c.execute("INSERT INTO posts (content) VALUES ('%s')" % content)
+    DB.commit()
+    DB.close()
