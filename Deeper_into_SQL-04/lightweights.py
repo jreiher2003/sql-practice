64407e2b3f04656f1abef7a5712744454c2043aa
Jeff Reiher <jeffreiher@gmail.com>
2015-09-17 09:34:02 -0400

    add a subquery

---
+++ Deeper_into_SQL-04/lightweights.py
@@ -0,0 +1,11 @@
+# Find the players whose weight is less than the average.
+# 
+# The function below performs two database queries in order to find the right players.
+# Refactor this code so that it performs only one query.
+#
+
+def lightweights(cursor):
+    """Returns a list of the players in the db whose weight is less than the average."""
+    cursor.execute("select name, weight from players, (select avg(weight) \
+    	as av from players)as subq where weight < av);
+    return cursor.fetchall()
