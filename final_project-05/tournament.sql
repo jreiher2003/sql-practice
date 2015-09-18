fc2d0fddc097baf4202a6ac43cad6db6025b5831
Jeff Reiher <jeffreiher@gmail.com>
2015-09-18 19:08:12 -0400

    created players table; name, id

---
+++ final_project-05/tournament.sql
@@ -0,0 +1,19 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+CREATE TABLE players (
+	name TEXT NOT NULL,
+	id INTEGER PRIMARY KEY AUTOINCREMENT);
+
+CREATE TABLE matchup (
+	player_1 TEXT,
+	player_2 TEXT,
+	win INTEGER,
+	loss INTEGER,
+	FOREIGN KEY(player_1) REFERENCES players(id),
+	FOREIGN KEY(player_2) REFERENCES players(id));
