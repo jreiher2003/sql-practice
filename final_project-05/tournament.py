e2e9d20c2f3bb0295e4cfb1e0812dff1669d0684
Jeff Reiher <jeffreiher@gmail.com>
2015-09-22 18:02:44 -0400

    add the rest of functions to complete project

--- final_project-05/tournament.py
+++ final_project-05/tournament.py
@@ -22,7 +22,7 @@ def close():
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    delP = c.execute('DELETE from players')
+    delP = c.execute('DELETE FROM players');
     print "All player records were successfully deleted."
     return delP
     
@@ -30,15 +30,19 @@ def deletePlayers():
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    c.execute("SELECT count(*) from players")
-    num = c.fetchall()[0][0]
-    try:
-        return c.fetchall()[0][0]
-    except IndexError:
-        if num == 1:
-            print "There is only %d player registered" % (num)
-        else:
-            print "There are %d players registered" % (num)
+    c.execute("SELECT count(*) FROM players");
+    num = c.fetchone()[0]
+    print num
+    if num == 1:
+        print "There is only %d player registered" % (num)
+    else:
+        print "There are %d players registered" % (num)
+    return num
+    # except IndexError:
+    #     if num == 1:
+    #         print "There is only %d player registered" % (num)
+    #     else:
+    #         print "There are %d players registered" % (num)
 
     
 
@@ -50,7 +54,7 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
-    regP = c.execute("INSERT INTO players (name) VALUES(?)", (name,)) # remember to make it a tuple
+    regP = c.execute("INSERT INTO players (name) VALUES(?)", (name,)); # remember to make it a tuple
     print "Successfully added player %s" % name
     return regP
 
@@ -61,61 +65,76 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    round = 1
-    mat = c.execute("INSERT INTO matchup (round,winner,loser)\
-                 VALUES(?,?,?)", (round,winner,loser))
-    print "In round %d, player id #%d is the winner and player id #%d is the loser." %(round,winner,loser)
-    return mat
+    c.execute("INSERT INTO matchup(winner,loser) VALUES (?,?)", (winner,loser,));
+    c.execute("UPDATE players SET wins = wins + 1, matches = matches + 1 WHERE id = ?", (winner,));
+    c.execute("UPDATE players SET matches=matches+1 WHERE id =?", (loser,));
+    print "Player #id %d was the winner and player #id %d was the loser" % (winner,loser)
+    return
 
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    delMat = c.execute("DELETE from matchup")
+    c.execute("DELETE FROM matchup");
     print "All matches have been successfully deleted"
-    return delMat
+    return 
 
 
-# def playerStandings():
-#     """Returns a list of the players and their win records, sorted by wins.
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
 
-#     The first entry in the list should be the player in first place, or a player
-#     tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
 
-#     Returns:
-#       A list of tuples, each of which contains (id, name, wins, matches):
-#         id: the player's unique id (assigned by the database)
-#         name: the player's full name (as registered)
-#         wins: the number of matches the player has won
-#         matches: the number of matches the player has played
-#     """
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+    c.execute("SELECT id,name,wins,matches FROM players ORDER BY wins DESC");
+    print c.fetchall()
+    return c.fetchall()
 
 
  
  
-# def swissPairings():
-#     """Returns a list of pairs of players for the next round of a match.
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
   
-#     Assuming that there are an even number of players registered, each player
-#     appears exactly once in the pairings.  Each player is paired with another
-#     player with an equal or nearly-equal win record, that is, a player adjacent
-#     to him or her in the standings.
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
   
-#     Returns:
-#       A list of tuples, each of which contains (id1, name1, id2, name2)
-#         id1: the first player's unique id
-#         name1: the first player's name
-#         id2: the second player's unique id
-#         name2: the second player's name
-#     """
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+    standings = playerStandings()
+    pairings = []
+    for i in range(0, len(standings), 2):
+        standing1 = standing[i]
+        standing2 = standing[i+1]
+        pairings.append([standing1[0], standing1[1], standing2[0], standing2[1]])
+    return pairings
 
 
 if __name__ == '__main__':
     conn, c = connect()
-    # registerPlayer('Jeff Reiher')
+    # # registerPlayer('Jeff Reiher')
     # registerPlayer('Molly Fig')
+    # registerPlayer('Paige Paiger')
+    # registerPlayer('Dumb Ass')
     # countPlayers()
     # deletePlayers()
-    # reportMatch(1,2)
-    deleteMatches()
+    reportMatch(1,2)
+    reportMatch(3,4)
+    # deleteMatches()
+    # playerStandings()
+    swissPairings()
     close()
